7ea8b08c983b6b53477d51f6cad696810790db1a
hinedy <ahmedhinedy@gmail.com>
2022-12-04 19:57:57 +0200

    Update Files

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ formEl.addEventListener('submit', (e)=>{
     e.preventDefault()
     let searchString = searchInput.value
     
-    requestUrl = `http://www.omdbapi.com/?s=${searchString}&type=movie&apikey=6a616cc4`
+    requestUrl = `https://www.omdbapi.com/?s=${searchString}&type=movie&apikey=6a616cc4`
     fetch(requestUrl)
         .then(res => res.json())
         .then(data => {
@@ -38,7 +38,7 @@ document.addEventListener('click', (e)=>{
 })
 
 function getMovieDetails(movieId){
-    requestUrl = `http://www.omdbapi.com/?i=${movieId}&type=movie&apikey=6a616cc4`
+    requestUrl = `https://www.omdbapi.com/?i=${movieId}&type=movie&apikey=6a616cc4`
     fetch(requestUrl)
         .then(res => res.json())
         .then(data => {
@@ -64,7 +64,7 @@ function updateResultsHtml(movie){
                     <p class="movie-runtime">${movie.Runtime}</p>
                     <p class="movie-genres">${movie.Genre}</p>
                     <button class="add-remove-btn" data-id=${movie.imdbID}>
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" fill="white"/>
                         </svg>
                         Watchlist
--- watchlist.js
+++ watchlist.js
@@ -29,7 +29,7 @@ function updateWatchlistHtml(movie){
                                 <p class="movie-runtime">${movie.Runtime}</p>
                                 <p class="movie-genres">${movie.Genre}</p>
                                 <button class="add-remove-btn" data-id=${movie.imdbID}>
-                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z" fill="white"/>
                                     </svg>
                                     Remove
@@ -67,7 +67,7 @@ function renderWatchlistApology(){
                 <div class="body-wrapper">
                     <h2 class="no-data">Your watchlist is looking a little empty...</h2>
                     <a href="index.html" class="page-nav">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" fill="white"/>
                         </svg>
                         Let&#39;s add some movies!
